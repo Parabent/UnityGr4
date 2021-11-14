4b462b8c3a7fc098634d9b982be1a2f1250b2d40
Parabent <47740800+Parabent@users.noreply.github.com>
2021-11-14 14:19:50 +0100

    fix zad2

--- Assets/OpenDoor.cs
+++ Assets/OpenDoor.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class OpenDoor : MonoBehaviour
 {
     public GameObject door;
+    public int version;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("naura");
         if (other.gameObject.tag == "Player")
         {
-            door.SendMessage("Open");
+            door.SendMessage("Open",version);
         }
 
 
--- Assets/Zadd2.cs
+++ Assets/Zadd2.cs
@@ -6,6 +6,8 @@ public class Zadd2 : MonoBehaviour
 {
     // Start is called before the first frame update
     public Vector3 vec;
+    public Vector3 left;
+    public Vector3 right;
     private void Start()
     {
        
@@ -21,8 +23,28 @@ public class Zadd2 : MonoBehaviour
      
 
     }
-    public void Open()
+    public void Open(int version)
     {
-        transform.Rotate(vec);
+        if(version == 1)
+        {
+            transform.Rotate(vec);
+        }
+        if(version == 2)
+        {
+            transform.position += left * Time.deltaTime;
+        }
+        if(version == 3)
+        {
+            transform.position += right * Time.deltaTime;
+        }
+      
     }
+    //public void Open2()
+    //{
+    //    transform.position += left * Time.deltaTime;
+    //}
+    //public void Open3()
+    //{
+    //    transform.position += right * Time.deltaTime;
+    //}
 }
